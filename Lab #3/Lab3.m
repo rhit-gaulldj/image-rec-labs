ba69841eabffa8545561066b7090c82a0ec560f2
rhit-beloremd <152201862+rhit-beloremd@users.noreply.github.com>
2024-12-13 14:53:23 -0500

    lab3 starter

--- Lab #3/Lab3.m
+++ Lab #3/Lab3.m
@@ -12,8 +12,25 @@
 %**************************************************************************
  
 img = imread('./nyc_times_square.jpg');
-imshow(img);
+%imshow(img);
+grayImg = rgb2gray(img);
+%imshow(grayImg);
 
 filt = ones(3,3)/9; % equivalent to [1/9 1/9 1/9;  1/9 1/9 1/9; 1/9 1/9 1/9]
 smoothImg = filter2(filt, grayImg); % applies the filter
-imtool(img); imtool(uint8(smoothImg));
+figure;
+imshow(smoothImg);
+
+gfilter = [1 4 6 4 1; 4 16 24 16 4;6 24 36 24 6; 4 16 24 16 4; 1 4 6 4 1]/256;
+%figure;
+%mesh(gfilter);
+trueGaussian = fspecial('gaussian', 5);
+%figure;
+%mesh(trueGaussian);
+
+true_filtered = imfilter(grayImg,trueGaussian);
+figure;
+imshow(true_filtered);
+approx_filtered = imfilter(grayImg, gfilter);
+figure;
+imshow(approx_filtered);
