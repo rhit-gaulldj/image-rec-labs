8a0d44944c1936f5a279d74eba3a54b61aec1c7c
Daniel Gaull <gaulldj@rose-hulman.edu>
2024-12-15 12:51:54 -0500

    Adding smoothing filters

--- Lab #3/Lab3.m
+++ Lab #3/Lab3.m
@@ -34,10 +34,50 @@ approx_filtered = imfilter(grayImg, gfilter);
 figure;
 imshow(approx_filtered);
 
+gfiltered = imfilter(grayImg, gfilter);
+uniform_filtered = imfilter(grayImg, [1 1 1; 1 1 1; 1 1 1]/9);
+
 [horizontal_edge, vertical_edge, sum, mag, direction, direction_strong] = sobel(grayImg);
 
 figure;
-subplot(2,4,1); imshow(img); title('Original')
+sgtitle('Unfiltered');
+subplot(2,4,1); imshow(grayImg); title('Original')
+subplot(2,4,2); imshow(horizontal_edge); title('Horizontal Filter')
+subplot(2,4,3); imshow(vertical_edge); title('Vertical Filter')
+subplot(2,4,4); imshow(sum); title('Sum')
+subplot(2,4,5); imshow(mag); title('Magnitude')
+subplot(2,4,6); imshow(direction); title('Direction')
+subplot(2,4,7); imshow(direction_strong); title('Direction Strong')
+
+[horizontal_edge, vertical_edge, sum, mag, direction, direction_strong] = sobel(gfiltered);
+
+figure;
+sgtitle('Approximate Gaussian Filter');
+subplot(2,4,1); imshow(gfiltered); title('Original')
+subplot(2,4,2); imshow(horizontal_edge); title('Horizontal Filter')
+subplot(2,4,3); imshow(vertical_edge); title('Vertical Filter')
+subplot(2,4,4); imshow(sum); title('Sum')
+subplot(2,4,5); imshow(mag); title('Magnitude')
+subplot(2,4,6); imshow(direction); title('Direction')
+subplot(2,4,7); imshow(direction_strong); title('Direction Strong')
+
+[horizontal_edge, vertical_edge, sum, mag, direction, direction_strong] = sobel(true_filtered);
+
+figure;
+sgtitle('Gaussian Filter');
+subplot(2,4,1); imshow(true_filtered); title('Original')
+subplot(2,4,2); imshow(horizontal_edge); title('Horizontal Filter')
+subplot(2,4,3); imshow(vertical_edge); title('Vertical Filter')
+subplot(2,4,4); imshow(sum); title('Sum')
+subplot(2,4,5); imshow(mag); title('Magnitude')
+subplot(2,4,6); imshow(direction); title('Direction')
+subplot(2,4,7); imshow(direction_strong); title('Direction Strong')
+
+[horizontal_edge, vertical_edge, sum, mag, direction, direction_strong] = sobel(uniform_filtered);
+
+figure;
+sgtitle('Uniform Filter');
+subplot(2,4,1); imshow(uniform_filtered); title('Original')
 subplot(2,4,2); imshow(horizontal_edge); title('Horizontal Filter')
 subplot(2,4,3); imshow(vertical_edge); title('Vertical Filter')
 subplot(2,4,4); imshow(sum); title('Sum')
