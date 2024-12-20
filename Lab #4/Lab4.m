227ffc4a6a42ec0f6633c3bd4fdce076438f3280
rhit-beloremd <152201862+rhit-beloremd@users.noreply.github.com>
2024-12-20 18:44:00 -0500

    Update Lab4.m
    fixed circularity calculation

--- Lab #4/Lab4.m
+++ Lab #4/Lab4.m
@@ -1,9 +1,27 @@
+%**************************************************************************
+% Lab4.m
+%
+%   PROGRAM DESCRIPTION
+%   see Lab4.docx
+%
+%   Output: 
+%   
+%
+%   Written by Maria D. Beloreshka & Daniel Gaull
+%   Date: 12/20/24
+%**************************************************************************
+
+clear variables;
+
 img = imread('shapes.png');
 img = img ~= 0;
 %imshow(img);
 shapes = bwlabel(img, 8);
 numShapes = max(max(shapes));
-for i = 0:numShapes
+for i = 1:numShapes
+    if nnz(shapes == i) == 0
+        continue;
+    end
     [r, c] = find(shapes == i);
     cov = [r(:), c(:)];
     n = max(size(r));
@@ -13,6 +31,8 @@ for i = 0:numShapes
     E = eig(cov);
     elongation = sqrt(max(E) / min(E));
     fprintf('Elongation of shape %d: %d\n', i, elongation);
-    circularity = sum(sum(shapes == i)) * 4 * pi / perimeter(shapes == i, 8);
+    circularity = sum(sum(shapes == i)) * 4 * pi / (nnz(bwperim(shapes == i, 8)))^2;
     fprintf('Circularity of shape %d: %d\n', i, circularity);
 end
+
+
