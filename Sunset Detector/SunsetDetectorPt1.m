2df995280e275c3da2151aac67627c0ca5e4a090
rhit-beloremd <152201862+rhit-beloremd@users.noreply.github.com>
2025-01-16 20:09:59 -0500

    Create SunsetDetectorPt1.m
    main file for pt. 1

---
+++ Sunset Detector/SunsetDetectorPt1.m
@@ -0,0 +1,12 @@
+%**************************************************************************
+% SunsetDetectorPt1.m
+%
+%   PROGRAM DESCRIPTION
+%   see Sunset Detector Pt. 1 project description
+%
+%   Output: 
+%
+%   Written by Daniel Gaull & Maria D. Beloreshka
+%   Date: 1/16/25
+%**************************************************************************
+ 
