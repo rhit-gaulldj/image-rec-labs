435e9d7fee1e315f630c6be14b9cbbcb5588a2a0
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-01-19 17:13:22 -0500

    Fixing code and some figure generation

--- Sunset Detector/SunsetDetectorPt1.m
+++ Sunset Detector/SunsetDetectorPt1.m
@@ -10,83 +10,94 @@
 %   Date: 1/16/25
 %**************************************************************************
 
-[Xtrain, ytrain] = getXY('images\train');
-[Xtest, ytest] = getXY('images\test');
-[Xvalid, yvalid] = getXY('images\validate');
+% [Xtrain, ytrain] = getXY('images\train');
+% [Xtest, ytest] = getXY('images\test');
+% [Xvalid, yvalid] = getXY('images\validate');
+% 
+% save('features.mat', 'Xtrain', 'ytrain', 'Xtest', 'ytest', 'Xvalid', 'yvalid');
+% 
+% load('features.mat');
+% 
+% C = 10.^(-5:1:10);
+% KScale = 10.^(-5:1:10);
+% 
+% datagrid = zeros(length(C) * length(KScale), 3);
+% 
+% index = 1;
+% for i=1:length(C)
+%     for j=1:length(KScale)
+%         c = C(i);
+%         k = KScale(j);
+%         model = fitcsvm(Xtrain,ytrain,'KernelFunction','RBF', 'Standardize',true, ...
+%             'ClassNames',[-1, 1], 'BoxConstraint',c,'KernelScale',k);
+%         [detectedClasses, distances] = predict(model, Xvalid);
+%         accuracy = sum(yvalid == detectedClasses) / size(yvalid, 1) * 100;
+%         datagrid(index, 1) = accuracy;
+%         datagrid(index, 2) = c;
+%         datagrid(index, 3) = k;
+%         index = index + 1;
+%     end
+%     fprintf("Finished C %d\n", i);
+% end
+% 
+% save('datagrid.mat', 'datagrid');
+% 
+% load('datagrid.mat');
+% [maxAccuracy, index] = max(datagrid(:,1));
+% maxAccuracy, index
+% 
+% % "Zoom in" on the hyperparameters
+% 
+% load('features.mat');
+% 
+% C = 1:1:20;
+% KScale = 1:1:20;
+% 
+% datagrid2 = zeros(length(C) * length(KScale), 3);
+% 
+% index = 1;
+% for i=1:length(C)
+%     for j=1:length(KScale)
+%         c = C(i);
+%         k = KScale(j);
+%         model = fitcsvm(Xtrain,ytrain,'KernelFunction','RBF', 'Standardize',true, ...
+%             'ClassNames',[-1, 1], 'BoxConstraint',c,'KernelScale',k);
+%         [detectedClasses, distances] = predict(model, Xvalid);
+%         accuracy = sum(yvalid == detectedClasses) / size(yvalid, 1) * 100;
+%         datagrid2(index, 1) = accuracy;
+%         datagrid2(index, 2) = c;
+%         datagrid2(index, 3) = k;
+%         index = index + 1;
+%     end
+%     fprintf("Finished C %d\n", i);
+% end
+% 
+% save('datagrid2.mat', 'datagrid2');
+% 
+% [maxAccuracy, index] = max(datagrid2(:,1));
+% maxAccuracy, index
 
-save('features.mat', 'Xtrain', 'ytrain', 'Xtest', 'ytest', 'Xvalid', 'yvalid');
+% BoxConstraint = 2
+% KernelScale = 13
 
-load('trainFeatures.mat');
-
-C = 10.^(-5:1:10);
-KScale = 10.^(-5:1:10);
-
-datagrid = zeros(length(C) * length(KScale), 3);
-
-index = 1;
-for i=1:length(C)
-    for j=1:length(KScale)
-        c = C(i);
-        k = KScale(j);
-        model = fitcsvm(Xtrain,ytrain,'KernelFunction','RBF', 'Standardize',true, ...
-            'ClassNames',[-1, 1], 'BoxConstraint',c,'KernelScale',k);
-        [detectedClasses, distances] = predict(model, Xtest);
-        accuracy = sum(ytest == detectedClasses) / size(ytest, 1) * 100;
-        datagrid(index, 1) = accuracy;
-        datagrid(index, 2) = c;
-        datagrid(index, 3) = k;
-        index = index + 1;
-    end
-    fprintf("Finished C %d\n", i);
-end
-
-save('datagrid.mat', 'datagrid');
-
-[maxAccuracy, index] = max(datagrid(:,1));
-maxAccuracy, index
-
-% "Zoom in" on the hyperparameters
-
-load('trainFeatures.mat');
-
-C = 500:100:1500;
-KScale = 90:5:110;
-
-datagrid2 = zeros(length(C) * length(KScale), 3);
-
-index = 1;
-for i=1:length(C)
-    for j=1:length(KScale)
-        c = C(i);
-        k = KScale(j);
-        model = fitcsvm(Xtrain,ytrain,'KernelFunction','RBF', 'Standardize',true, ...
-            'ClassNames',[-1, 1], 'BoxConstraint',c,'KernelScale',k);
-        [detectedClasses, distances] = predict(model, Xtest);
-        accuracy = sum(ytest == detectedClasses) / size(ytest, 1) * 100;
-        datagrid2(index, 1) = accuracy;
-        datagrid2(index, 2) = c;
-        datagrid2(index, 3) = k;
-        index = index + 1;
-    end
-    fprintf("Finished C %d\n", i);
-end
-
-save('datagrid2.mat', 'datagrid2');
-
-[maxAccuracy, index] = max(datagrid2(:,1));
-maxAccuracy, index
-
-% BoxConstraint = 1000
-% KernelScale = 95
+load('features.mat');
 
 % Define the best parameters found
-bestC = 1000; % Update as per your hyperparameter tuning
-bestKScale = 95; % Update as per your hyperparameter tuning
+bestC = 2; % Update as per your hyperparameter tuning
+bestKScale = 13; % Update as per your hyperparameter tuning
 
 % Train the SVM model with the best hyperparameters
 model = fitcsvm(Xtrain, ytrain, 'KernelFunction', 'RBF', ...
     'Standardize', true, 'ClassNames', [-1, 1], ...
     'BoxConstraint', bestC, 'KernelScale', bestKScale);
+
+fprintf('Support vectors: %f, percent: %f\n', size(model.SupportVectors, 1), size(model.SupportVectors, 1) / size(Xtrain, 1));
+[detectedClasses, ~] = predict(model, Xvalid);
+accuracy = sum(yvalid == detectedClasses) / size(yvalid, 1) * 100;
+fprintf('Validation Accuracy: %f\n', accuracy);
+[detectedClasses, distances] = predict(model, Xtest);
+accuracy = sum(ytest == detectedClasses) / size(ytest, 1) * 100;
+fprintf('Test Accuracy: %f\n', accuracy);
 
 % Predict the scores for the test data
 [~, scores] = predict(model, Xtest); 
---
+++ Sunset Detector/plots.m
@@ -0,0 +1,31 @@
+load('datagrid.mat');
+
+accs = datagrid(:, 1);
+Cs = datagrid(:, 2);
+Ks = datagrid(:, 3);
+
+figure;
+scatter(Cs, Ks, 50, accs, 'filled');
+set(gca, 'XScale', 'log', 'YScale', 'log');
+colorbar;
+colormap jet;
+xlabel('Box Constraint (log scale)');
+ylabel('Kernel Scale (log scale)');
+title('Hyperparameter Tuning with Validation Accuracy (Part 1)');
+
+%%%%%%%
+
+load('datagrid2.mat');
+
+accs = datagrid2(:, 1);
+Cs = datagrid2(:, 2);
+Ks = datagrid2(:, 3);
+
+figure;
+scatter(Cs, Ks, 50, accs, 'filled');
+%set(gca, 'XScale', 'log', 'YScale', 'log');
+colorbar;
+colormap jet;
+xlabel('Box Constraint (linear scale)');
+ylabel('Kernel Scale (linear scale)');
+title('Hyperparameter Tuning with Validation Accuracy (Part 2)');
