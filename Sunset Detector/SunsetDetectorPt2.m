06ecd3c6e5323366173038e64f672bd37cb82457
rhit-beloremd <152201862+rhit-beloremd@users.noreply.github.com>
2025-01-23 20:18:07 -0500

    initialized file

---
+++ Sunset Detector/SunsetDetectorPt2.m
@@ -0,0 +1,11 @@
+%**************************************************************************
+% SunsetDetectorPt2.m
+%
+%   PROGRAM DESCRIPTION
+%   see Sunset Detector Pt. 2 project description
+%
+%   Output: 
+%
+%   Written by Daniel Gaull & Maria D. Beloreshka
+%   Date: 01/23/25
+%**************************************************************************
