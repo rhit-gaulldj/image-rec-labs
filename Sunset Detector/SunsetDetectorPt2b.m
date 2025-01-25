56ef42a1a23e0c40e1f9742ef0bfcfa7ace798ae
Daniel Gaull <gaulldj@rose-hulman.edu>
2025-01-25 15:39:35 -0500

    Created part 2b file

---
+++ Sunset Detector/SunsetDetectorPt2b.m
@@ -0,0 +1,13 @@
+%**************************************************************************
+% SunsetDetectorPt2b.m
+%
+%   PROGRAM DESCRIPTION
+%   see Sunset Detector Pt. 2b project description
+%
+%   Output: 
+%
+%   Written by Daniel Gaull & Maria D. Beloreshka
+%   Date: 01/25/25
+%**************************************************************************
+
+
